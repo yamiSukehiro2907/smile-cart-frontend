c2d50b420c4dd3e967fb5888a2ffd72c27ab5b13
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2026-02-07 15:02:03 +0530

    Initialized query client instace

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
+import queryClient from "utils/queryClient";
 
 import App from "./App";
 import "./common/i18n";
@@ -14,9 +16,11 @@ import initializeAxios from "apis/axios";
 initializeAxios();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
